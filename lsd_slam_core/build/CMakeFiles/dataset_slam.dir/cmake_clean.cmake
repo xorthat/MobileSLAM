aa00d9c9a7bfbd52e6fdcfb9b55f61c6cc22436e
Jennifer Lake <jennifer.e.lake@gmail.com>
2016-11-23 10:54:02 -0500

    Removed unnecessary files

---
+++ lsd_slam_core/build/CMakeFiles/dataset_slam.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dataset_slam.dir/src/main_on_images.cpp.o"
+  "../bin/dataset_slam.pdb"
+  "../bin/dataset_slam"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dataset_slam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lsd_slam_core/build/CMakeFiles/lsdslam.dir/cmake_clean.cmake
@@ -0,0 +1,30 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lsdslam.dir/src/DataStructures/Frame.cpp.o"
+  "CMakeFiles/lsdslam.dir/src/DataStructures/FramePoseStruct.cpp.o"
+  "CMakeFiles/lsdslam.dir/src/DataStructures/FrameMemory.cpp.o"
+  "CMakeFiles/lsdslam.dir/src/SlamSystem.cpp.o"
+  "CMakeFiles/lsdslam.dir/src/LiveSLAMWrapper.cpp.o"
+  "CMakeFiles/lsdslam.dir/src/DepthEstimation/DepthMap.cpp.o"
+  "CMakeFiles/lsdslam.dir/src/DepthEstimation/DepthMapPixelHypothesis.cpp.o"
+  "CMakeFiles/lsdslam.dir/src/util/globalFuncs.cpp.o"
+  "CMakeFiles/lsdslam.dir/src/util/SophusUtil.cpp.o"
+  "CMakeFiles/lsdslam.dir/src/util/settings.cpp.o"
+  "CMakeFiles/lsdslam.dir/src/util/Undistorter.cpp.o"
+  "CMakeFiles/lsdslam.dir/src/Tracking/Sim3Tracker.cpp.o"
+  "CMakeFiles/lsdslam.dir/src/Tracking/Relocalizer.cpp.o"
+  "CMakeFiles/lsdslam.dir/src/Tracking/SE3Tracker.cpp.o"
+  "CMakeFiles/lsdslam.dir/src/Tracking/TrackingReference.cpp.o"
+  "CMakeFiles/lsdslam.dir/src/IOWrapper/Timestamp.cpp.o"
+  "CMakeFiles/lsdslam.dir/src/GlobalMapping/FabMap.cpp.o"
+  "CMakeFiles/lsdslam.dir/src/GlobalMapping/KeyFrameGraph.cpp.o"
+  "CMakeFiles/lsdslam.dir/src/GlobalMapping/g2oTypeSim3Sophus.cpp.o"
+  "CMakeFiles/lsdslam.dir/src/GlobalMapping/TrackableKeyFrameSearch.cpp.o"
+  "CMakeFiles/lsdslam.dir/src/IOWrapper/OpenCV/ImageDisplay_OpenCV.cpp.o"
+  "../lib/liblsdslam.pdb"
+  "../lib/liblsdslam.dylib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lsdslam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
