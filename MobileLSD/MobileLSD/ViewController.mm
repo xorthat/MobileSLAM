3fb8a95a23571f299be8005ac71b068095a0b659
polltooh <polltooh.gmail>
2016-11-26 14:19:42 -0500

    example to run

--- MobileLSD/MobileLSD/ViewController.mm
+++ MobileLSD/MobileLSD/ViewController.mm
@@ -8,8 +8,11 @@
 
 #import "ViewController.h"
 #include <boost/thread.hpp>
-#include <boost/chrono.hpp>
+#include "util/settings.h"
+#include "util/globalFuncs.h"
+#include "opencv2/opencv.hpp"
 #include <iostream>
+#include "SlamSystem.h"
 
 
 boost::mutex mutex;
@@ -24,10 +27,21 @@ int count = 0;
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    boost::thread t1;
-    boost::thread_group threads;
-    for (int i = 0; i < 3; ++i)
-        threads.create_thread(&increment_count);
+    //boost::thread t1;
+    //boost::thread_group threads;
+    //for (int i = 0; i < 3; ++i)
+    //    threads.create_thread(&increment_count);
+	float fx = 905;
+	float fy = 2144;
+	float cx = 540;
+	float cy = 960;
+	Sophus::Matrix3f K;
+	K << fx, 0.0, cx, 0.0, fy, cy, 0.0, 0.0, 1.0;
+	float w = 480;
+	float h = 640;
+	bool doSlam = false;
+	lsd_slam::SlamSystem* system = new lsd_slam::SlamSystem(w, h, K, doSlam);
+	delete system;
 
 }
 
